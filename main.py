4d1ed7ba53b3873f461ba5c8721c8022c1afd412
CDulouard <clement.dulouard@gmail.com>
2019-01-24 16:19:54 +0100

    nouvelles map

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import modules.Map as Map
 import modules.Pathfinding as pf
+import sys
 
 
-def main():
-    map = Map.Map("map")
-    pf.find(1, map)
+def main(args):
+    map = Map.Map("maps/Map_1.txt")
+    print(pf.find(1, map, False))
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
--- modules/Pathfinding.py
+++ modules/Pathfinding.py
@@ -6,8 +6,11 @@ def find(nbants, map, show=True):
     Fonction de recherche de solution à modifier pour trouver le meilleur chemin
     :param nbants: nombre de fourmis a tester
     :param map: map de jeu
-    :return:
+    :return: Nombre de mouvements moyens
     """
+    avgmove = 0
+    for i in range(nbants):
+        myant = Ant.Ant()
+        avgmove += myant.find_path(map, show)
 
-    myant = Ant.Ant()
-    print(myant.find_path(map, show))
+    return avgmove / nbants
